98436d88c6a4f721bc3ded90317c00e885559645
1 <>
2023-09-04 19:46:22 +0300

    master: added initial unit test

---
+++ src/main/java/dk/danske/fx/rates/CurrencyLabels.java
@@ -0,0 +1,7 @@
+package dk.danske.fx.rates;
+
+public class CurrencyLabels {
+    public static final String EURO = "Euro";
+    public static final String DANISH_KRONE = "Danish krone";
+    public static final String SWEDISH_KRONA = "Swedish krona";
+}
--- src/test/java/dk/danske/fx/rates/FxRatesServiceTest.java
+++ src/test/java/dk/danske/fx/rates/FxRatesServiceTest.java
@@ -1,7 +1,48 @@
 package dk.danske.fx.rates;
 
+import dk.danske.fx.rates.provider.FxRatesException;
+import dk.danske.fx.rates.provider.FxRatesProvider;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 class FxRatesServiceTest {
 
-    // TODO: add tests
+    @Mock
+    private FxRatesProvider fxRatesProvider;
+    @InjectMocks
+    private FxRatesService fxRatesService;
 
+    @Test
+    void shouldFetchEurFxRateFromDkkSuccessfully() throws FxRatesException {
+        try (MockedStatic<LocalDateTime> utilities = mockStatic(LocalDateTime.class)) {
+            LocalDateTime expectedLocalDateTime = LocalDateTime.now();
+            utilities.when(LocalDateTime::now).thenReturn(expectedLocalDateTime);
+            LocalDate fetchDate = LocalDate.of(2023, 9, 4);
+            Currencies expectedCurrency = Currencies.DKK;
+            String expectedCurrencyLabel = CurrencyLabels.DANISH_KRONE;
+            double expectedRate = 7.45d;
+            when(fxRatesProvider.fetchRate(fetchDate, Currencies.EUR.toString(),
+                    Currencies.DKK.toString())).thenReturn(expectedRate);
+
+            FxRate actualFxRate = fxRatesService.fetchEurFxRate(fetchDate, expectedCurrency);
+
+            assertThat(actualFxRate.getRate()).isEqualTo(expectedRate);
+            assertThat(actualFxRate.getCurrency()).isEqualTo(expectedCurrency);
+            assertThat(actualFxRate.getCurrencyLabel()).isEqualTo(expectedCurrencyLabel);
+            assertThat(actualFxRate.getValueDateTime()).isEqualTo(expectedLocalDateTime);
+        }
+    }
 }
